5b5ae8cabe1a3b7ddd1eb409be8f49d7df70d41e
Blindeen <kuba.szmajda@gmail.com>
2025-07-11 19:11:34 +0200

    create toolbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [ToolbarComponent],
 })
 export class AppComponent {
   title = 'browser-runner';
