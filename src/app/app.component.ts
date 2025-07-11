d1ed674327cfd160fe8ffd88adbad5ccf3a94060
Blindeen <kuba.szmajda@gmail.com>
2025-07-11 19:22:22 +0200

    create footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ToolbarComponent],
+  imports: [ToolbarComponent, FooterComponent],
 })
-export class AppComponent {
-  title = 'browser-runner';
-}
+export class AppComponent {}
