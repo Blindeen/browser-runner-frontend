25dad23b6456b660f4462002cbd11af14b44e8cb
Blindeen <kuba.szmajda@gmail.com>
2025-07-18 13:25:05 +0200

    configure http client

--- src/app/editor/editor.service.ts
+++ src/app/editor/editor.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { signal } from '@angular/core';
+import { signal, inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EditorService {
   private code = signal('');
+  private httpClient = inject(HttpClient);
 
   updateCode(code: string) {
     this.code.set(code);
