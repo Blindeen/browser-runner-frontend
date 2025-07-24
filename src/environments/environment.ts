f908327cdd5c146f732e2d6f7864346e17c13b47
Blindeen <kuba.szmajda@gmail.com>
2025-07-24 11:55:30 +0200

    prepare environmental variables for export

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   apiUrl: 'http://localhost:3000/api',
   codeFallback: "console.log('Hello World!');\n",
   codeKey: 'code',
+  exportFilename: 'browser-runner-code.js',
 };
