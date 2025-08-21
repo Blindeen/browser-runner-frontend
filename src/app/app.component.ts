5c36ae66f2dd50173cfaa9544d5b835727d3e0a5
Blindeen <kuba.szmajda@gmail.com>
2025-08-21 12:39:45 +0200

    render loader conditionally, remove useless styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { HeaderComponent } from './editor/header/header.component';
 import { FooterComponent } from './editor/footer/footer.component';
 import { CodeEditorComponent } from './editor/code-editor/code-editor.component';
 import { ModalComponent } from './editor/modal/modal.component';
+import { LoaderComponent } from './loader/loader.component';
+import { ApiService } from './editor/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +16,9 @@ import { ModalComponent } from './editor/modal/modal.component';
     FooterComponent,
     CodeEditorComponent,
     ModalComponent,
+    LoaderComponent,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  apiService = inject(ApiService);
+}
